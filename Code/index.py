db2dae1bb44a8534b7942cde9d18ff4a603db5cf
prakharpande04 <144222667+prakharpande04@users.noreply.github.com>
2024-04-04 08:26:37 +0000

    Main windows and buttons added

--- Code/index.py
+++ Code/index.py
@@ -7,4 +7,15 @@ from tkinter import messagebox
 from tkcalendar import Calendar
 from tix import *
 
-
+def account():
+    global w
+    global w1
+    global lgn
+    b4=Button(w,text='Register',height='1',width='15',bg='darkorange',font=('Calibri bold',20),
+              cursor='hand2',command=signup)
+    b4.place(x=380,y=520)
+    
+    b5=Button(w,text='Login',height='1',width='15',bg='lime',font=('Calibri bold',20),
+              cursor='hand2',command=login)
+    b5.place(x=690,y=520)
+account()
