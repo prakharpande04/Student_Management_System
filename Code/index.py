3a78b6956042814838f3ee037f2a9206f08cbbb9
prakharpande04 <144222667+prakharpande04@users.noreply.github.com>
2024-04-04 08:24:33 +0000

    libraries installed and added

--- Code/index.py
+++ Code/index.py
@@ -1,1 +1,10 @@
+from tkinter import *
+from datetime import *
+from pytz import *
+from tkinter import ttk
+from tkinter.ttk import Style
+from tkinter import messagebox
+from tkcalendar import Calendar
+from tix import *
+
 
